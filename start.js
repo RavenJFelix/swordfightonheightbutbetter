68474e3d89af205524508695e07a1d39bfa36ff6
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-11-01 15:31:54 -0800

    initialize with second commit
    I am a true revolutionary

---
+++ start.js
@@ -0,0 +1,20 @@
+const nh = require('node-hill')
+
+nh.startServer({
+    gameId: 478, // Your game id here
+
+    port: 42480, // Your port id here (default is 42480)
+
+    local: false, // Whether or not your server is local
+
+    map: './maps/example.brk', // Your .brk file path
+
+    scripts: './user_scripts', // Your .js files path
+
+    // Add npm / built-in node.js modules here
+    modules: [
+        //"discord.js"
+    ]
+})
+
+// For more help: https://brickhill.gitlab.io/open-source/node-hill/interfaces/gamesettings.html
